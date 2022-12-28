10bd1edd0d361d093e0c7f97cf792d83c4d3e51a
Duduf <git@duduf.com>
2022-12-28 14:45:58 +0100

    update version info

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_BUILD 0
+#define VERSION_BUILD 1
 #define VERSION_SUFFIX "Beta"
 
 #define STRINGIFY_VERSION(A, B, C) CONCAT(A, B, C )
@@ -19,19 +19,18 @@
 #define VER_PRODUCTVERSION VERSION_MAJOR,VERSION_MINOR,VERSION_BUILD,0
 #define STR_PRODUCTVERSION STR_VERSION
 
-#define STR_COMPANYNAME "Rainbox Laboratory"
+#define STR_COMPANYNAME "RxLaboratory"
 #define STR_FILEDESCRIPTION "DuBuilder"
 #define STR_INTERNALNAME "DuBuilder"
-#define STR_LEGALCOPYRIGHT "Copyright (c) 2020 Rainbox Laboratory, Nicolas Dufresne and contributors"
+#define STR_LEGALCOPYRIGHT "Copyright (c) 2020-2022 RxLaboratory, Nicolas Dufresne and contributors"
 #define STR_LEGALTRADEMARKS1 "All Rights Reserved"
 #define STR_ORIGINALFILENAME "DuBuilder.exe"
 #define STR_PRODUCTNAME "DuBuilder - JSX(inc) scripts builder"
 
-#define STR_COMPANYDOMAIN "rainboxlab.org"
+#define STR_COMPANYDOMAIN "rxlaboratory.org"
 
-#define URL_CHAT "http://chat.rainboxlab.org"
-#define URL_FORUM "https://forum.rainboxlab.org"
-#define URL_DOC "https://dubuilder-docs.rainboxlab.org"
+#define URL_CHAT "http://chat.rxlab.info"
+#define URL_DOC "https://dubuilder.rxlab.guide"
 #define URL_BUGREPORT "https://github.com/Rainbox-dev/DuAEF_DuBuilder/issues/new/choose"
 
 #endif // VERSION_H
